f1547d7a0fbcd4eaa4a9685a69cd80e7f035b0ee
n0k5 <hey@chrish.design>
2019-10-10 09:49:02 +0100

    Remove unused items

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Header from './header'
 
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
+    const { children } = this.props
+    // const rootPath = `${__PATH_PREFIX__}/`
 
     return (
       <div>
