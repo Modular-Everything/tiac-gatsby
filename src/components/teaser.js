ae10de5586b37e82a7ca48003a5363bb6aaa537f
n0k5 <hey@chrish.design>
2019-10-11 08:28:55 +0100

    Quick bit of styling for this

--- src/components/teaser.js
+++ src/components/teaser.js
@@ -4,18 +4,23 @@ import SbEditable from 'storyblok-react'
 
 const Teaser = props => (
   <SbEditable content={props.blok}>
-    <div className="jumbotron jumbotron-fluid">
-      <div className="container">
-        <h1 className="display-4">{props.blok.headline}</h1>
-        <p className="lead">
-          This is a modified jumbotron that occupies the entire horizontal space
-          of its parent.
-        </p>
-        <p className="lead">
-          <Link className="btn btn-primary" to={'/blog/'}>
-            View More
-          </Link>
-        </p>
+    <div className="w-full bg-gray-200 p-6">
+      <h1 className="text-gray-800 font-bold text-3xl mb-2">
+        {props.blok.headline}
+      </h1>
+      <p className="mb-8 text-gray-700 text-sm max-w-lg">
+        This is a modified jumbotron that occupies the entire horizontal space
+        of its parent. This is a modified jumbotron that occupies the entire
+        horizontal space of its parent.This is a modified jumbotron that
+        occupies the entire horizontal space of its parent.
+      </p>
+      <div>
+        <Link
+          className="py-3 px-5 rounded-sm uppercase text-sm text-white font-bold leading-relaxed bg-teal-500 shadow-md inline-block"
+          to={'/blog/'}
+        >
+          View More
+        </Link>
       </div>
     </div>
   </SbEditable>
