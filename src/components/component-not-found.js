09ef6e01b285a889f8105253e72739d598570554
n0k5 <hey@chrish.design>
2019-10-13 07:02:23 +0100

    Some styling to pop this out as an error

--- src/components/component-not-found.js
+++ src/components/component-not-found.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const ComponentNotFound = props => (
-  <div>
+  <div className="container border border-dashed border-red-600 bg-red-100 my-6 py-6">
     Component {props.blok.component} is not defined. Make sure it's added to{' '}
-    <code>components.js</code>
+    <code className="text-red-800">components.js</code>
   </div>
 )
 
