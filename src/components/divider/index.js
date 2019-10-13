67c8b1b117aacbd96336200a71d9e997d87f910c
n0k5 <hey@chrish.design>
2019-10-13 16:53:05 +0100

    Add SbEditable to divider

--- src/components/divider/index.js
+++ src/components/divider/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import SbEditable from 'storyblok-react'
 
 const Divider = props => (
-  <div className="container mb-12">
-    <hr className="border-brand-gray-300" />
-  </div>
+  <SbEditable content={props.blok}>
+    <div className="container mb-12">
+      <hr className="border-brand-gray-300" />
+    </div>
+  </SbEditable>
 )
 
 export default Divider
