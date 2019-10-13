a4e1ae8bc16ee175872becfc41af305783be1e38
n0k5 <hey@chrish.design>
2019-10-13 09:52:21 +0100

    Tidy up

--- src/components/divider/index.js
+++ src/components/divider/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
-const Divider = props => {
-  const color = props.colour
-
-  return (
-    <div className="container mb-12">
-      <hr className="border-brand-gray-300" />
-    </div>
-  )
-}
+const Divider = props => (
+  <div className="container mb-12">
+    <hr className="border-brand-gray-300" />
+  </div>
+)
 
 export default Divider
