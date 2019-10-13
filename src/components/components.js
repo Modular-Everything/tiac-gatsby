0df7977bfb56fa041ad407d63140f438c5dadee4
n0k5 <hey@chrish.design>
2019-10-13 07:03:52 +0100

    Add markdown styling

--- src/components/components.js
+++ src/components/components.js
@@ -3,6 +3,7 @@ import Grid from './grid'
 import Teaser from './teaser'
 import Feature from './feature'
 import Slider from './slider'
+import Markdown from './markdown'
 import ComponentNotFound from './component-not-found'
 
 const ComponentList = {
@@ -11,6 +12,7 @@ const ComponentList = {
   teaser: Teaser,
   feature: Feature,
   slider: Slider,
+  markdown: Markdown,
 }
 
 const Components = type => {
---
+++ src/components/markdown/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import SbEditable from 'storyblok-react'
+import * as markdownit from 'markdown-it'
+import './markdown.css'
+
+const Markdown = props => {
+  console.table(props.blok)
+
+  const md = markdownit()
+  md.use(require('markdown-it-highlightjs'))
+  const htmlContent = md.render(props.blok.copy)
+
+  return (
+    <SbEditable content={props.blok}>
+      <div
+        className="markdownContent container"
+        dangerouslySetInnerHTML={{ __html: htmlContent }}
+      />
+    </SbEditable>
+  )
+}
+
+export default Markdown
