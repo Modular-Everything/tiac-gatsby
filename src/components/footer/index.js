0bc3187d073b34422e7612c51af4f3e6b5b3ceb9
n0k5 <hey@chrish.design>
2019-10-18 20:14:32 +0100

    Clean this up

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,23 +5,39 @@ const Footer = props => (
     <div className="container py-12">
       <ul className="flex justify-center">
         <li className="relative mr-6 md:mr-8 lg:mr-12">
-          <i className="absolute right-0 left-0 text-center">temp.</i>
-          <a href="http://instagram.com">Instagram</a>
+          <i className="absolute right-0 left-0 text-center opacity-0 hover:opacity-1">
+            temp.
+          </i>
+          <a href="http://instagram.com" className="hover:opacity-0">
+            Instagram
+          </a>
         </li>
 
         <li className="relative mr-6 md:mr-8 lg:mr-12">
-          <i className="absolute right-0 left-0 text-center">temp.</i>
-          <a href="http://twitter.com">Twitter</a>
+          <i className="absolute right-0 left-0 text-center opacity-0 hover:opacity-1">
+            temp.
+          </i>
+          <a href="http://twitter.com" className="hover:opacity-0">
+            Twitter
+          </a>
         </li>
 
         <li className="relative mr-6 md:mr-8 lg:mr-12">
-          <i className="absolute right-0 left-0 text-center">temp.</i>
-          <a href="http://linkedin.com">LinkedIn</a>
+          <i className="absolute right-0 left-0 text-center opacity-0 hover:opacity-1">
+            temp.
+          </i>
+          <a href="http://linkedin.com" className="hover:opacity-0">
+            LinkedIn
+          </a>
         </li>
 
         <li className="relative">
-          <i className="absolute right-0 left-0 text-center">temp.</i>
-          <a href="http://behance.com">Behance</a>
+          <i className="absolute right-0 left-0 text-center opacity-0 hover:opacity-1">
+            temp.
+          </i>
+          <a href="http://behance.com" className="hover:opacity-0">
+            Behance
+          </a>
         </li>
       </ul>
     </div>
