b298642f35d38cb4fee7bbbf0234ce507122cbe5
n0k5 <hey@chrish.design>
2019-10-18 16:24:51 +0100

    Fix video breaking grid layout

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -3,11 +3,9 @@ import React from 'react'
 const Video = props => {
   return (
     <div className={`rows-${props.blok.rows} cols-${props.blok.columns}`}>
-      <div className="">
-        <video height="100%" width="100%" autoPlay loop muted>
-          <source src={props.blok.source} type="video/mp4"></source>
-        </video>
-      </div>
+      <video height="100%" width="100%" autoPlay loop muted>
+        <source src={props.blok.source} type="video/mp4"></source>
+      </video>
       {props.blok.caption !== '' ? (
         <h4 className="mt-3 text-brand-gray-600 text-xs tracking-wide lg:text-sm">
           {props.blok.caption}
