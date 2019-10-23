de2d0b7407287e6c688e8dfafbeeea656ed8f76b
n0k5 <hey@chrish.design>
2019-10-23 22:05:06 +0100

    Fix missing video bug

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import SbEditable from 'storyblok-react'
 
 const Video = props => {
   return (
     <div className={`rows-${props.blok.rows} cols-${props.blok.columns}`}>
-      <video height="100%" width="100%" autoPlay loop muted>
-        <source src={props.blok.source} type="video/mp4"></source>
-      </video>
+      <div className="relative">
+        <SbEditable content={props.blok}>
+          <video height="100%" width="100%" autoPlay loop muted>
+            <source src={props.blok.video} type="video/mp4"></source>
+          </video>
+        </SbEditable>
+
+        {/* {props.blok.hover_caption !== '' ? (
+          <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-brand-black text-white text-xs opacity-75">
+            <p>{props.blok.hover_caption}</p>
+          </div>
+        ) : (
+          ''
+        )} */}
+      </div>
+
       {props.blok.caption !== '' ? (
-        <h4 className="mt-3 text-brand-gray-600 text-xs tracking-wide lg:text-sm">
+        <h4
+          className={`mt-3 cen text-brand-gray-600 text-xs text-${
+            props.blok.caption_align ? props.blok.caption_align : 'left'
+          } lg:text-sm tracking-wide`}
+        >
           {props.blok.caption}
         </h4>
       ) : (
