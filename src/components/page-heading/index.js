4f000c83e77aa00bca8ddb0d2ac04c149650bb39
n0k5 <hey@chrish.design>
2019-10-28 07:05:10 +0000

    Update heading on homepage

--- src/components/page-heading/index.js
+++ src/components/page-heading/index.js
@@ -38,10 +38,23 @@ const PageHeading = props => {
   return (
     <div className="container py-6 sm:py-12 md:py-16">
       <div className="flex flex-col-reverse justify-between sm:flex-row sm:flex-1">
-        <h1 className="text-brand-black text-xl mt-2 sm:mt-0 sm:text-xl sm:flex sm:flex-col sm:justify-center md:text-2xl">
-          {isHome ? data.allStoryblokSpace.edges[0].node.name : props.pageName}
-        </h1>
-        <ul className="flex flex-row sm:justify-end">{categories}</ul>
+        {isHome ? (
+          <>
+            <h1 className="hidden text-brand-black text-xl mt-2 sm:mt-0 sm:text-xl sm:flex sm:flex-col sm:justify-center md:text-2xl">
+              {data.allStoryblokSpace.edges[0].node.name}
+            </h1>
+            <p className="text-sm max-w-xs">
+              Designing &amp; directing across brand identity, screen and print.
+            </p>
+          </>
+        ) : (
+          <>
+            <h1 className="text-brand-black text-xl mt-2 sm:mt-0 sm:text-xl sm:flex sm:flex-col sm:justify-center md:text-2xl">
+              {props.pageName}
+            </h1>
+            <ul className="flex flex-row sm:justify-end">{categories}</ul>
+          </>
+        )}
       </div>
     </div>
   )
