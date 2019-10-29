8f370b0e1053bba7ec93d1cf625832a28c71ab8d
n0k5 <hey@chrish.design>
2019-10-29 07:14:04 +0000

    Wrap SbEditable around caption

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -3,35 +3,35 @@ import SbEditable from 'storyblok-react'
 
 const Video = props => {
   return (
-    <div className={`rows-${props.blok.rows} cols-${props.blok.columns}`}>
-      <div className="relative">
-        <SbEditable content={props.blok}>
+    <SbEditable content={props.blok}>
+      <div className={`rows-${props.blok.rows} cols-${props.blok.columns}`}>
+        <div className="relative">
           <video height="100%" width="100%" autoPlay loop muted>
             <source src={props.blok.video} type="video/mp4"></source>
           </video>
-        </SbEditable>
 
-        {/* {props.blok.hover_caption !== '' ? (
+          {/* {props.blok.hover_caption !== '' ? (
           <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-brand-black text-white text-xs opacity-75">
-            <p>{props.blok.hover_caption}</p>
+          <p>{props.blok.hover_caption}</p>
           </div>
+          ) : (
+            ''
+          )} */}
+        </div>
+
+        {props.blok.caption !== '' ? (
+          <h4
+            className={`mt-3 cen text-brand-gray-600 text-xxs text-${
+              props.blok.caption_align ? props.blok.caption_align : 'left'
+            } lg:text-sm tracking-wide`}
+          >
+            {props.blok.caption}
+          </h4>
         ) : (
           ''
-        )} */}
+        )}
       </div>
-
-      {props.blok.caption !== '' ? (
-        <h4
-          className={`mt-3 cen text-brand-gray-600 text-xxs text-${
-            props.blok.caption_align ? props.blok.caption_align : 'left'
-          } lg:text-sm tracking-wide`}
-        >
-          {props.blok.caption}
-        </h4>
-      ) : (
-        ''
-      )}
-    </div>
+    </SbEditable>
   )
 }
 
