b9334e9fa60ff9c8f8cb4d8a323817fffe45370d
n0k5 <hey@chrish.design>
2019-11-13 19:32:59 +0000

    Only show credits on 51-53 page (temporary)

--- src/components/credits/index.js
+++ src/components/credits/index.js
@@ -2,44 +2,48 @@ import React from 'react'
 import './credits.css'
 
 const Credits = props => {
-  // console.log(props)
-  // var credits = []
+  if (props.pageSlug === '51-53') {
+    // console.log(props)
+    // var credits = []
 
-  // for (const [index, value] of props.credit) {
-  //   credits.push(<li key={index}>{value.credit_name}</li>)
-  // }
+    // for (const [index, value] of props.credit) {
+    //   credits.push(<li key={index}>{value.credit_name}</li>)
+    // }
 
-  return (
-    <div className="credits container">
-      <h2>Credits</h2>
-      <ul>
-        <li>
-          <ul>
-            <li className="credits__name">Andy Cooke</li>
-            <li className="credits__role">Designer</li>
-          </ul>
-        </li>
-        <li>
-          <ul>
-            <li className="credits__name">Andy Cooke</li>
-            <li className="credits__role">Designer</li>
-          </ul>
-        </li>
-        <li>
-          <ul>
-            <li className="credits__name">Andy Cooke</li>
-            <li className="credits__role">Designer</li>
-          </ul>
-        </li>
-        <li>
-          <ul>
-            <li className="credits__name">Andy Cooke</li>
-            <li className="credits__role">Designer</li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  )
+    return (
+      <div className="credits container">
+        <h2>Credits</h2>
+        <ul>
+          <li>
+            <ul>
+              <li className="credits__name">Andy Cooke</li>
+              <li className="credits__role">Designer</li>
+            </ul>
+          </li>
+          <li>
+            <ul>
+              <li className="credits__name">Andy Cooke</li>
+              <li className="credits__role">Designer</li>
+            </ul>
+          </li>
+          <li>
+            <ul>
+              <li className="credits__name">Andy Cooke</li>
+              <li className="credits__role">Designer</li>
+            </ul>
+          </li>
+          <li>
+            <ul>
+              <li className="credits__name">Andy Cooke</li>
+              <li className="credits__role">Designer</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 
 export default Credits
