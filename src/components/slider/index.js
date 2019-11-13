78c29ffeac313461e24f4ffbb7bc10208e85dc6e
n0k5 <hey@chrish.design>
2019-11-13 19:53:34 +0000

    Reduce container padding on mobile

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -45,7 +45,7 @@ const Slider = props => {
                   <Img fixed={fluidProps} />
                 </div>
                 {hasCaption ? (
-                  <p className="text-brand-gray-600 text-center text-xxs tracking-wide leading-loose mt-16 mb-0">
+                  <p className="text-brand-gray-600 text-center text-xxs tracking-wide leading-loose mt-16 mb-0 px-4">
                     {slide.caption}
                   </p>
                 ) : (
