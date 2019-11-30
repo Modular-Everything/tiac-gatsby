8fe051ffff8f64c77854279ebfb6e3a4aa9e8d99
n0k5 <hey@chrish.design>
2019-11-30 13:23:59 +0000

    Fix invalid syntax

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -84,14 +84,8 @@ const Header = () => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <title>Menu-Icon</title>
-              <g
-                id="Page-1"
-                stroke="none"
-                stroke-width="1"
-                fill="none"
-                fill-rule="evenodd"
-              >
-                <g id="Group" fill="#EFEFEF" fill-rule="nonzero">
+              <g>
+                <g id="Group" fill="#EFEFEF" fillRule="nonzero">
                   <path
                     d="M0.5,0 L28.5,0 C28.776142,0 29,0.2238576 29,0.5 C29,0.7761424 28.776142,1 28.5,1 L0.5,1 C0.223858,1 0,0.7761424 0,0.5 C0,0.2238576 0.223858,0 0.5,0 Z M0.5,7 L28.5,7 C28.776142,7 29,7.2238576 29,7.5 C29,7.7761424 28.776142,8 28.5,8 L0.5,8 C0.223858,8 0,7.7761424 0,7.5 C0,7.2238576 0.223858,7 0.5,7 Z"
                     id="Menu-Icon"
