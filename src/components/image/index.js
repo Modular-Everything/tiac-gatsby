787271bfb39494a68fa9a1a30a9b8535842074c5
n0k5 <hey@chrish.design>
2019-11-30 14:50:03 +0000

    Fix missing image bug

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -5,41 +5,51 @@ import Img from 'gatsby-image'
 import { getFluidGatsbyImage } from 'gatsby-storyblok-image'
 
 const Image = props => {
-  const fluidProps = getFluidGatsbyImage(props.blok.image, {
-    maxWidth: 1000,
-  })
+  console.log(props.blok.image)
 
-  return (
-    <SbEditable content={props.blok}>
-      <div className={`rows-${props.blok.rows} cols-${props.blok.columns}`}>
-        <div className="relative">
-          <div className="relative overflow-hidden">
-            <Img fixed={fluidProps} />
+  if (props.blok.image) {
+    const fluidProps = getFluidGatsbyImage(props.blok.image, {
+      maxWidth: 1000,
+    })
+
+    return (
+      <SbEditable content={props.blok}>
+        <div className={`rows-${props.blok.rows} cols-${props.blok.columns}`}>
+          <div className="relative">
+            <div className="relative overflow-hidden">
+              <Img fixed={fluidProps} />
+            </div>
+
+            {/* {props.blok.hover_caption !== undefined ? (
+              <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-brand-black opacity-75 text-white text-xs">
+                <p>{props.blok.hover_caption}</p>
+              </div>
+            ) : (
+              ''
+            )} */}
           </div>
 
-          {/* {props.blok.hover_caption !== undefined ? (
-            <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-brand-black opacity-75 text-white text-xs">
-              <p>{props.blok.hover_caption}</p>
-            </div>
+          {props.blok.caption !== '' ? (
+            <h4
+              className={`mt-3 cen text-brand-gray-600 text-xxs text-${
+                props.blok.caption_align ? props.blok.caption_align : 'left'
+              } lg:text-sm tracking-wide`}
+            >
+              {props.blok.caption}
+            </h4>
           ) : (
             ''
-          )} */}
+          )}
         </div>
-
-        {props.blok.caption !== '' ? (
-          <h4
-            className={`mt-3 cen text-brand-gray-600 text-xxs text-${
-              props.blok.caption_align ? props.blok.caption_align : 'left'
-            } lg:text-sm tracking-wide`}
-          >
-            {props.blok.caption}
-          </h4>
-        ) : (
-          ''
-        )}
-      </div>
-    </SbEditable>
-  )
+      </SbEditable>
+    )
+  } else {
+    return (
+      <p className="container border border-dashed border-red-600 bg-red-100 my-6 py-6">
+        Add an image asset
+      </p>
+    )
+  }
 }
 
 export default Image
