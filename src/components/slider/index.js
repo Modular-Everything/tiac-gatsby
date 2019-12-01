c643bc649905e6e2a9f63025aa2a3ffc9becb6a4
n0k5 <hey@chrish.design>
2019-12-01 17:56:44 +0000

    Fix slides not initiatlising and causing mayhem

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -10,10 +10,12 @@ import './slider.css'
 const Slider = props => {
   if (props.blok.slides.length === 1) {
     var params = {
-      init: false,
+      init: true,
+      pagination: false,
+      effect: 'fade',
     }
   } else {
-    params = {
+    var params = {
       init: true,
       loop: true,
       lazy: false,
@@ -24,44 +26,52 @@ const Slider = props => {
     }
   }
 
-  return (
-    <SbEditable content={props.blok}>
-      <div className={props.blok.container ? 'container' : ''}>
-        <Swiper {...params}>
-          {props.blok.slides.map((slide, index) => {
-            let fluidProps = ''
-            if (slide.image !== 'undefined' && slide.image !== '') {
-              fluidProps = getFluidGatsbyImage(slide.image, {
-                maxWidth: 1000,
-                maxHeight: 585,
-              })
-            }
+  if (props.blok.slides.length >= 1 && props.blok.slides[0].image) {
+    return (
+      <SbEditable content={props.blok}>
+        <div className={props.blok.container ? 'container' : ''}>
+          <Swiper {...params}>
+            {props.blok.slides.map((slide, index) => {
+              let fluidProps = ''
+              if (slide.image !== 'undefined' && slide.image !== '') {
+                fluidProps = getFluidGatsbyImage(slide.image, {
+                  maxWidth: 1000,
+                  maxHeight: 585,
+                })
+              }
 
-            if (slide.caption === '') {
-              var hasCaption = false
-            } else {
-              hasCaption = true
-            }
+              if (slide.caption === '') {
+                var hasCaption = false
+              } else {
+                hasCaption = true
+              }
 
-            return (
-              <div key={index}>
-                <div>
-                  <Img fluid={fluidProps} />
+              return (
+                <div key={index}>
+                  <div>
+                    <Img fluid={fluidProps} />
+                  </div>
+                  {hasCaption ? (
+                    <p className="text-brand-gray-600 text-center text-xxs tracking-wide leading-loose mt-10 mb-0 px-4">
+                      {slide.caption}
+                    </p>
+                  ) : (
+                    ''
+                  )}
                 </div>
-                {hasCaption ? (
-                  <p className="text-brand-gray-600 text-center text-xxs tracking-wide leading-loose mt-10 mb-0 px-4">
-                    {slide.caption}
-                  </p>
-                ) : (
-                  ''
-                )}
-              </div>
-            )
-          })}
-        </Swiper>
-      </div>
-    </SbEditable>
-  )
+              )
+            })}
+          </Swiper>
+        </div>
+      </SbEditable>
+    )
+  } else {
+    return (
+      <p className="container border border-dashed border-red-600 bg-red-100 my-6 py-6">
+        Add an asset
+      </p>
+    )
+  }
 }
 
 export default Slider
