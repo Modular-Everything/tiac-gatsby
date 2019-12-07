b2c312f2ee4ef0afdee39c25b6337f2129137e29
n0k5 <hey@chrish.design>
2019-12-07 17:05:10 +0000

    Better error handling on credits

--- src/components/credits/index.js
+++ src/components/credits/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import './credits.css'
 
 const Credits = props => {
-  var credits = []
-  for (const [index, value] of props.who.entries()) {
-    credits.push(
-      <li key={index}>
-        <ul>
-          <li className="credits__name">{value.credit.credit_name}</li>
-          <li className="credits__role">{value.credit.credit_role}</li>
-        </ul>
-      </li>
-    )
-  }
+  if (typeof props.who !== 'undefined') {
+    var credits = []
+    for (const [index, value] of props.who.entries()) {
+      credits.push(
+        <li key={index}>
+          <ul>
+            <li className="credits__name">{value.credit.credit_name}</li>
+            <li className="credits__role">{value.credit.credit_role}</li>
+          </ul>
+        </li>
+      )
+    }
 
-  if (props.who.length !== 0) {
     return (
       <div className="bg-white py-12">
         <div className="credits container">
