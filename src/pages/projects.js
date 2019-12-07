99d507c90cbbb0ea07c0ab1dcba86cadb5294342
n0k5 <hey@chrish.design>
2019-12-07 10:54:34 +0000

    Add tags to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,13 +8,14 @@ const Projects = () => {
   const data = useStaticQuery(
     graphql`
       query Projects {
-        allStoryblokEntry {
+        allStoryblokEntry(filter: { parent_id: { eq: 2875854 } }) {
           edges {
             node {
               name
               full_slug
               parent_id
               field_cover_string
+              tag_list
               published_at
             }
           }
@@ -32,36 +33,41 @@ const Projects = () => {
             var isVideo = true
           }
 
-          if (node.parent_id === 2875854) {
-            return (
-              <div
-                key={index}
-                className={`${
-                  !isVideo ? 'aspect-ratio-43/25' : ''
-                } relative overflow-hidden`}
-              >
-                <Link to={node.full_slug}>
-                  <h2 className="text-xs sm:text-white mb-2 sm:absolute sm:flex sm:justify-center sm:items-center sm:w-full sm:h-full sm:mb-0 sm:z-10 sm:p-2 sm:bg-brand-black-overlay sm:opacity-0 sm:hover:opacity-100 transition-opacity transition-ease-in-out">
-                    {node.name}
-                  </h2>
-                  {!isVideo ? (
-                    <img
-                      src={node.field_cover_string}
-                      alt=""
-                      className="absolute top-0 left-0 w-full h-full object-cover"
-                    />
-                  ) : (
-                    <video height="100%" width="100%" autoPlay loop muted>
-                      <source
-                        src={node.field_cover_string}
-                        type="video/mp4"
-                      ></source>
-                    </video>
-                  )}
-                </Link>
-              </div>
+          var tags = []
+          for (const [index, value] of node.tag_list.entries()) {
+            tags.push(
+              <li key={index} className="sm:mb-1 last:mb-0">
+                {value}
+              </li>
             )
           }
+
+          return (
+            <div key={index} className="relative overflow-hidden">
+              <Link to={node.full_slug}>
+                <div className="hidden sm:visible sm:absolute sm:flex sm:justify-center sm:flex-col sm:items-center sm:w-full sm:h-full sm:z-10 sm:p-2 sm:bg-brand-black-overlay sm:opacity-0 sm:hover:opacity-100 transition-opacity transition-ease-in-out">
+                  <h2 className="text-sm sm:text-white mb-2">{node.name}</h2>
+                  <ul className="text-xs sm:text-white sm:text-center">
+                    {tags}
+                  </ul>
+                </div>
+                {!isVideo ? (
+                  <img
+                    src={node.field_cover_string}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                ) : (
+                  <video height="100%" width="100%" autoPlay loop muted>
+                    <source
+                      src={node.field_cover_string}
+                      type="video/mp4"
+                    ></source>
+                  </video>
+                )}
+              </Link>
+            </div>
+          )
         })}
       </div>
     </Layout>
