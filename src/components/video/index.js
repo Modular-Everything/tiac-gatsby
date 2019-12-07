9335e908d534e8ed43b7ab187f11c8add668255c
n0k5 <hey@chrish.design>
2019-12-07 11:27:14 +0000

    Fix autoBuffer syntax

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -6,15 +6,7 @@ const Video = props => {
     <SbEditable content={props.blok}>
       <div className={`rows-${props.blok.rows} cols-${props.blok.columns}`}>
         <div className="relative">
-          <video
-            height="100%"
-            width="100%"
-            autoPlay
-            loop
-            autobuffer
-            muted
-            playsinline
-          >
+          <video height="100%" width="100%" autoPlay loop muted playsInline>
             <source src={props.blok.video} type="video/mp4"></source>
           </video>
 
