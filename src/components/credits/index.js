3e611b6867acb59efe782c7227b6f2b7fe1d7d76
n0k5 <hey@chrish.design>
2019-12-07 17:13:05 +0000

    Fix another bug

--- src/components/credits/index.js
+++ src/components/credits/index.js
@@ -3,26 +3,30 @@ import './credits.css'
 
 const Credits = props => {
   if (typeof props.who !== 'undefined') {
-    var credits = []
-    for (const [index, value] of props.who.entries()) {
-      credits.push(
-        <li key={index}>
-          <ul>
-            <li className="credits__name">{value.credit.credit_name}</li>
-            <li className="credits__role">{value.credit.credit_role}</li>
-          </ul>
-        </li>
-      )
-    }
+    if (props.who.length !== 0) {
+      var credits = []
+      for (const [index, value] of props.who.entries()) {
+        credits.push(
+          <li key={index}>
+            <ul>
+              <li className="credits__name">{value.credit.credit_name}</li>
+              <li className="credits__role">{value.credit.credit_role}</li>
+            </ul>
+          </li>
+        )
+      }
 
-    return (
-      <div className="bg-white py-12">
-        <div className="credits container">
-          <h2>Credits</h2>
-          <ul>{credits}</ul>
+      return (
+        <div className="bg-white py-12">
+          <div className="credits container">
+            <h2>Credits</h2>
+            <ul>{credits}</ul>
+          </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return null
+    }
   } else {
     return null
   }
