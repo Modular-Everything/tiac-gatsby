b7dac976e0d35f25bd4d6e9852bafc63f6246bf9
n0k5 <hey@chrish.design>
2019-12-08 14:58:40 +0000

    Add clickable images

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,34 +1,29 @@
 import React from 'react'
 import SbEditable from 'storyblok-react'
+import { Link } from 'gatsby'
 import './image.css'
-// import Img from 'gatsby-image'
-// import { getFluidGatsbyImage } from 'gatsby-storyblok-image'
 
 const Image = props => {
   if (props.blok.image) {
-    // const fluidProps = getFluidGatsbyImage(props.blok.image.toLowerCase(), {
-    //   maxWidth: 1000,
-    // })
-
     const imageService = '//img2.storyblok.com/'
     const path = props.blok.image.replace('//a.storyblok.com', '')
     const height = props.blok.rows === 'two' ? '1530' : '746'
     const width = props.blok.columns === 'two' ? '2496' : '1232'
     const resizedImage = imageService + width + 'x' + height + path
 
+    const imageLink = props.blok.image_link ? (
+      <Link to={`/` + props.blok.image_link.cached_url}>
+        <img src={resizedImage} alt="" width={width / 2} height={height / 2} />
+      </Link>
+    ) : (
+      <img src={resizedImage} alt="" width={width / 2} height={height / 2} />
+    )
+
     return (
       <SbEditable content={props.blok}>
         <div className={`rows-${props.blok.rows} cols-${props.blok.columns}`}>
           <div className="relative">
-            <div className="relative overflow-hidden">
-              {/* <Img fixed={fluidProps} /> */}
-              <img
-                src={resizedImage}
-                alt=""
-                width={width / 2}
-                height={height / 2}
-              />
-            </div>
+            <div className="relative overflow-hidden">{imageLink}</div>
 
             {/* {props.blok.hover_caption !== undefined ? (
               <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-brand-black opacity-75 text-white text-xs">
