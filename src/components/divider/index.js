d52d271fdf56a956a4c2b8778db839d6f02ffc7c
n0k5 <hey@chrish.design>
2019-12-11 07:45:45 +0000

    Invert the divider toggle

--- src/components/divider/index.js
+++ src/components/divider/index.js
@@ -4,7 +4,7 @@ import SbEditable from 'storyblok-react'
 const Divider = props => (
   <SbEditable content={props.blok}>
     <div className="container mb-16 md:mb-24">
-      {props.blok.divider === true ? (
+      {props.blok.divider !== true ? (
         <hr className="border-brand-gray-300" />
       ) : (
         <hr className="border-transparent" />
