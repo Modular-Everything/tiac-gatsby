d9e5cce60903c8a7dcb1a7742e6233830a756c62
n0k5 <hey@chrish.design>
2020-01-13 21:15:15 +0000

    [+] Add gtag via plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,8 +43,13 @@ module.exports = {
         icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: `UA-155565849-1`,
+        head: false,
+        anonymize: true,
+      },
+    },
   ],
 }
