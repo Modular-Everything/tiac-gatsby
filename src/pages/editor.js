464a73efd75a7266d5f019b0891517b013ee7735
Chrish Dunne <hey@chrish.design>
2020-03-19 09:30:14 +0000

    [~] Update to functional component

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -1,82 +1,55 @@
-import React from 'react'
-import Layout from '../components/layout'
-import Components from '../components/components.js'
-import SbEditable from 'storyblok-react'
+import React, { useState, useEffect, useCallback } from 'react'
 import config from '../../gatsby-config'
+import StoryblokComponents from '../components/components'
+
+// ============================================================================
 
 const sbConfigs = config.plugins.filter(item => {
-  return item.resolve === 'gatsby-source-storyblok'
+  return item.resolve === `gatsby-source-storyblok`
 })
 const sbConfig = sbConfigs.length > 0 ? sbConfigs[0] : {}
 
-const loadStoryblokBridge = function(cb) {
-  let script = document.createElement('script')
+// ============================================================================
+
+const LoadStoryblokBridge = cb => {
+  const script = document.createElement('script')
   script.type = 'text/javascript'
   script.src = `//app.storyblok.com/f/storyblok-latest.js?t=${sbConfig.options.accessToken}`
   script.onload = cb
   document.getElementsByTagName('head')[0].appendChild(script)
 }
 
-// eslint-disable-next-line
-const getParam = function(val) {
-  var result = ''
-  var tmp = []
+// ============================================================================
 
-  window.location.search
-    .substr(1)
-    .split('&')
-    .forEach(function(item) {
-      tmp = item.split('=')
-      if (tmp[0] === val) {
-        result = decodeURIComponent(tmp[1])
-      }
-    })
+const StoryblokEntry = () => {
+  const [story, setStory] = useState()
 
-  return result
-}
-
-class StoryblokEntry extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { story: null }
-  }
-
-  componentDidMount() {
-    loadStoryblokBridge(() => {
-      this.initStoryblokEvents()
-    })
-  }
-
-  loadStory() {
+  const loadStory = useCallback(() => {
     window.storyblok.get(
       {
-        slug: window.storyblok.getParam('path'),
-        version: 'draft',
+        slug: window.storyblok.getParam(`path`),
+        version: `draft`,
         resolve_relations: sbConfig.options.resolveRelations || [],
       },
       data => {
-        this.setState({ story: data.story })
+        setStory(data.story)
       }
     )
-  }
+  })
 
-  initStoryblokEvents() {
-    this.loadStory()
+  const initStoryblokEvents = useCallback(() => {
+    loadStory()
 
-    let sb = window.storyblok
+    const sb = window.storyblok
 
-    sb.on(['change', 'published'], payload => {
-      this.loadStory()
+    sb.on(['change', 'published'], () => {
+      loadStory()
     })
 
-    sb.on('input', payload => {
-      if (this.state.story && payload.story.id === this.state.story.id) {
-        payload.story.content = sb.addComments(
-          payload.story.content,
-          payload.story.id
-        )
-        this.setState({ story: payload.story })
-      }
+    sb.on(['input'], payload => {
+      const pl = payload
+      pl.story.content = sb.addComments(pl.story.content, pl.story.id)
+      setStory(payload.story)
     })
 
     sb.pingEditor(() => {
@@ -84,28 +57,31 @@ class StoryblokEntry extends React.Component {
         sb.enterEditmode()
       }
     })
+  }, [loadStory, story])
+
+  useEffect(() => {
+    LoadStoryblokBridge(() => {
+      initStoryblokEvents()
+    })
+  }, [])
+
+  if (!story) {
+    return <div />
   }
 
-  render() {
-    if (this.state.story == null) {
-      return <div></div>
-    }
+  const { content } = story
 
-    let content = this.state.story.content
-
-    return (
-      <Layout>
-        <SbEditable content={content}>
-          <>
-            {React.createElement(Components(content.component), {
-              key: content._uid,
-              blok: content,
-            })}
-          </>
-        </SbEditable>
-      </Layout>
-    )
-  }
+  return (
+    <>
+      {React.createElement(StoryblokComponents(content.component), {
+        // eslint-disable-next-line no-underscore-dangle
+        key: content._uid,
+        blok: content,
+      })}
+    </>
+  )
 }
+
+// ============================================================================
 
 export default StoryblokEntry
