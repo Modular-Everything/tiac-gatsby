1455ceee1dec01d23bd457478e940557778132cc
Christian Dunne <yo@modular-everything.com>
2024-04-24 09:44:03 +0100

    fix?: Build error

--- src/pages/private-projects.js
+++ src/pages/private-projects.js
@@ -1,30 +1,36 @@
-import React from 'react'
-import { Router } from '@reach/router'
-import Layout from '../components/layout'
-import PrivateProjectsAuthd from '../components/private-projects/PrivateProjects'
-import Login from '../components/private-projects/Login'
-import PrivateRoute from '../components/PrivateRoute'
-import { isLoggedIn } from '../utils/auth'
-import { navigate } from 'gatsby'
+// import React from 'react'
+// import { Router } from '@reach/router'
+// import Layout from '../components/layout'
+// import PrivateProjectsAuthd from '../components/private-projects/PrivateProjects'
+// import Login from '../components/private-projects/Login'
+// import PrivateRoute from '../components/PrivateRoute'
+// import { isLoggedIn } from '../utils/auth'
+// import { navigate } from 'gatsby'
+
+// const PrivateProjects = () => {
+//   if (isLoggedIn()) {
+//     navigate(`/private-projects/list`)
+//   } else {
+//     navigate(`/private-projects/login`)
+//   }
+
+//   return (
+//     <Layout>
+//       <Router>
+//         <PrivateRoute
+//           path="/private-projects/list"
+//           component={PrivateProjectsAuthd}
+//         />
+//         <Login path="/private-projects/login" />
+//       </Router>
+//     </Layout>
+//   )
+// }
+
+// export default PrivateProjects
 
 const PrivateProjects = () => {
-  if (isLoggedIn()) {
-    navigate(`/private-projects/list`)
-  } else {
-    navigate(`/private-projects/login`)
-  }
-
-  return (
-    <Layout>
-      <Router>
-        <PrivateRoute
-          path="/private-projects/list"
-          component={PrivateProjectsAuthd}
-        />
-        <Login path="/private-projects/login" />
-      </Router>
-    </Layout>
-  )
+  return <div>test</div>
 }
 
 export default PrivateProjects
